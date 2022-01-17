d39176f139c7151ae13f17fe2cbe1c1024b0493e
Hélder Gonçalves <helderpimentaxc@gmail.com>
2022-01-17 22:03:11 +0000

    Initial commit

---
+++ app/src/main/java/com/example/brightcove/kotlin/videoplayer/VideoListActivity.kt
@@ -0,0 +1,46 @@
+package com.example.brightcove.kotlin.videoplayer
+
+import android.app.PendingIntent.getActivity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.Menu
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.brightcove.player.model.Video
+import com.example.brightcove.kotlin.videoplayer.viewmodels.PlayerListViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class VideoListActivity : AppCompatActivity(), VideoListActions {
+    private val viewModel by viewModels<PlayerListViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_list)
+
+        setupFragment()
+
+        viewModel.videoToLoad.observe(this, EventObserver { openVideoPlayer(it) })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, VideoListFragment())
+            .commitNow()
+    }
+
+    override fun openVideoPlayer(video: Video) {
+        val intent = Intent(this, VideoPlayerActivity::class.java)
+        intent.putExtra(VideoPlayerActivity.VIDEO_INTENT_DATA, video as Parcelable)
+        startActivity(intent)
+    }
+}
+
+interface VideoListActions {
+    fun openVideoPlayer(video: Video)
+}
